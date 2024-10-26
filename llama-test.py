d3da3a4dc59706611759381272dae3a3b245b59b
ebm010101 <98239326+ebm2023@users.noreply.github.com>
2024-10-25 18:04:13 -0700

    Add files via upload

--- llama-test.py
+++ llama-test.py
@@ -1,5 +1,23 @@
-# Load model directly
-from transformers import AutoProcessor, AutoModelForPreTraining
+import requests
+import torch
+from PIL import Image
+from transformers import MllamaForConditionalGeneration, AutoProcessor
 
-processor = AutoProcessor.from_pretrained("meta-llama/Llama-3.2-11B-Vision")
-model = AutoModelForPreTraining.from_pretrained("meta-llama/Llama-3.2-11B-Vision")
+model_id = "meta-llama/Llama-3.2-11B-Vision"
+
+model = MllamaForConditionalGeneration.from_pretrained(
+    model_id,
+    torch_dtype=torch.bfloat16,
+    low_cpu_mem_usage=True,
+    device_map="auto",
+)
+processor = AutoProcessor.from_pretrained(model_id)
+
+url = "https://www.bankrate.com/brp/2023/08/22121220/chart.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+prompt = "<|image|><|begin_of_text|>What is this"
+inputs = processor(image, prompt, return_tensors="pt").to(model.device)
+
+output = model.generate(**inputs, max_new_tokens=25)
+print(processor.decode(output[0]))
